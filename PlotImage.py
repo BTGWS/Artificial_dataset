359cfc363ea1ff8406099716cdeccfabdf7822a5
BTG <664933002@qq.com>
2021-04-26 19:29:21 +0800

    Complete the function of Display

--- PlotImage.py
+++ PlotImage.py
@@ -20,38 +20,53 @@
                   ┃┫┫  ┃┫┫
                   ┗┻┛  ┗┻┛
 """
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
 
-def Display(X1, X2, imageName):
+# 绘图函数
+def Display(dataset, Y, save_path):
+    # 绘制散点图
     """
-    :param X1:          数据集1
-    :param X2:          数据集2
-    :param imageName:   图片名
+    :param dataset:     numpy数组格式的源数据集
+    :param Y:           类别
+    :param save_path:   图片存储路径
     :return:
     """
-    # 绘图函数
     plt.rcParams['font.sans-serif'] = ['SimHei']
     plt.rcParams['axes.unicode_minus'] = False
     # matplotlib画图中中文显示会有问题，需要这两行设置默认字体
 
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.xlim(xmax=5, xmin=-1)
-    plt.ylim(ymax=5, ymin=-1)
+    plt.xlabel('x1')
+    plt.ylabel('x2')
+    # plt.xlim(xmax=4.5,xmin=-1.5)
+    # plt.ylim(ymax=2.5,ymin=0.5)
     # 画两条（-1,5）的坐标轴并设置轴标签x，y
+    colors = ['#00CED1', '#DC143C', 'k', 'limegreen', '#000000', '#006400', 'peru', 'c', 'orange', 'lightpink']
 
-    x_1, y_1, _ = X1.T
-    x_2, y_2, _ = X2.T
-    colors1 = '#00CED1'  # 点的颜色
-    colors2 = '#DC143C'
     area = np.pi * 4 ** 2  # 点面积
+
+    GA_S = np.array(dataset)
+    markers = ['+', '_', 'o', 'v', '<', '*', 'p', 'D', '^', '>']
+    index = 0
+    for i in Y:
+        # i = int(i)
+        # color=plt.cm.Set1(GA_S[i,2] / 10.)
+        x_1, x_2, c = GA_S[np.where(GA_S[:, 2] == i)].T
+        plt.scatter(x_1, x_2, s=area, color=colors[index], alpha=0.4, label='类别: ' + str(i), marker=markers[index])
+        index += 1
+
+    # x_1,y_1,c_1 = GA_S[np.where(GA_S[:,2] == 1)].T
+    # x_2,y_2,c_2 = GA_S[np.where(GA_S[:,2] == 2)].T
+
     # 画散点图
-    plt.scatter(x_1, y_1, s=area, c=colors1, alpha=0.4, label='类别1', marker='+')
-    plt.scatter(x_2, y_2, s=area, c=colors2, alpha=0.4, label='类别2', marker='_')
+    # plt.scatter(x_1, y_1, s=area, c=colors1, alpha=0.4, label='类别1', marker='+')
+    # plt.scatter(x_2, y_2, s=area, c=colors2, alpha=0.4, label='类别2', marker='_')
+    # plt.scatter(a1_test, Y_test, s=area, c=colors3, alpha=0.4, label='Training goal', marker='o')
+    # plt.scatter(a1_test, Fl_test+0.05, s=area, c=colors4, alpha=0.4, label='ELM output', marker='v')
+
     # plt.plot([-1,4.5],[4.5,-1],linewidth = '0.5',color='#000000')
     plt.legend()
-    plt.savefig(imageName + '.jpg', dpi=600)
-    # plt.show()
+    plt.savefig(save_path, dpi=600)
     plt.clf()
+    # plt.show()
--- Run_Main.py
+++ Run_Main.py
@@ -24,16 +24,15 @@ import numpy as np
 import pandas as pd
 
 from Continuous_dataset import Gaussian_Distribution_Dataset
-from IOFile import writeFile
+from IOFile import writeFile, read_File
 from PlotImage import Display
 
 if __name__ == '__main__':
-    num = 500
     mean_1 = [1.0, 1.0]
     cov_1 = [[0.6, -0.2], [-0.2, 0.6]]
-
     mean_2 = [2.5, 2.5]
     cov_2 = [[0.2, -0.1], [-0.1, 0.2]]
+    num = 500
 
     X1 = Gaussian_Distribution_Dataset(mean_1, cov_1, num, '1')
     X2 = Gaussian_Distribution_Dataset(mean_2, cov_2, num, '2')
@@ -41,4 +40,9 @@ if __name__ == '__main__':
     dataset = pd.DataFrame(ans, columns=['a1', 'a2', 'c'])
     # 写入文件
     writeFile(dataset, 'fuck_1')
-    Display(X1, X2, 'fuck_1')
+    # 读取文件
+    dataset = read_File('fuck_1.xlsx')
+    dataset = np.array(dataset)
+    # 数据可视化
+    labels = set(dataset[:, -1])
+    Display(dataset, list(labels), 'fuck_1')
